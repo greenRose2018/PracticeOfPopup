06a43cd559fb5e6933dfe7230d119493c3683209
greenRose2018 <brifun13@gmail.com>
2017-12-13 22:56:34 -0600

    added getters and setters, also made toString method

--- src/model/Colors.java
+++ src/model/Colors.java
@@ -21,3 +21,52 @@ public class Colors
 		this.isNot = isIt;
 		this.number = howMany;
 	}
+	public String getColor()
+	{
+		return color;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean getIsNot()
+	{
+		return isNot;
+	}
+	
+	public int getNumber()
+	{
+		return number;
+	}
+	
+	public void setColor(String color)
+	{
+		this.color = color;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setIsNot(boolean isNot)
+	{
+		this.isNot = isNot;
+	}
+	
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+	
+	public String toString()
+	{
+		String response = "";
+		response += "Hello my name is " + getName() + ". \n" + "I am the color " + getColor() + ". \n";
+		response += "I am " + getNumber() + " years old.\n";
+		return response;
+		
+	}
+}
