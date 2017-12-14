c68819802cf66c3db1455687ce578d071d6983eb
greenRose2018 <brifun13@gmail.com>
2017-12-13 22:54:10 -0600

    imported popupdisplay to controller , declared and initailized popupdisplay

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,20 @@
 package controller;
+
+import view.PopupDisplay;
+
 public class Controller 
 {
+	//Data members
+	private PopupDisplay popup;
+	
 	public Controller()
 	{
+		popup = new PopupDisplay();
+		
 	}
 	public void start()
 	{
+		popup.display("Hello");
+		
 	}
 }
