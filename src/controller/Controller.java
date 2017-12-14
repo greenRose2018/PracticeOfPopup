791953b44de32fc72bab1a850dc21be0b5519129
greenRose2018 <brifun13@gmail.com>
2017-12-13 22:58:37 -0600

    imported colors to Controller, and declared and intialized , tested(works)

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,20 +1,24 @@
 package controller;
 
 import view.PopupDisplay;
+import model.Colors;
 
 public class Controller 
 {
 	//Data members
 	private PopupDisplay popup;
+	private Colors color;
 	
 	public Controller()
 	{
 		popup = new PopupDisplay();
+		color = new Colors("blue","bob", false,5);
 		
 	}
 	public void start()
 	{
 		popup.display("Hello");
 		
+		popup.display(color.toString());
 	}
 }
